6b079bc2e4e724066f4b2ab0677771154daebeaf
Kamesh <Kamesh@DESKTOP-44Q126V>
2024-03-06 20:35:10 +0530

    87

--- src/ex/ex1.java
+++ src/ex/ex1.java
@@ -5,7 +5,7 @@ public class ex1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Kamesh");
-		System.out.println("tony");
+		System.out.println("tojjny");
 		
 		System.out.println("tony3");
 
